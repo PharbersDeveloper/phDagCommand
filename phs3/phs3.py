21a2579942001715b3a7c1899642a9372f5735d1
AlfredYang1986 <yangyuanpig@gmail.com>
2020-07-07 14:19:25 +0800

    修改

--- phs3/phs3.py
+++ phs3/phs3.py
@@ -27,7 +27,8 @@ class PhS3(object):
         if sys.version_info > (3, 0):
             return str.split(response["Body"].read().decode(), "\n")
         else:
-            return string.split(response["Body"].read().decode(), "\n")
+            res = response["Body"].read()
+            return string.split(res, "\n")
 
     def copy_object_2_file(self, bk_name, s3_path, local_path):
         f = open(local_path, "w")
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r") as fh:
 
 setuptools.setup(
     name="phcli",
-    version="0.0.24",
+    version="0.0.26",
     author="Alfred Yang",
     author_email="alfredyang@pharbers.com",
     description="pharbers dag scheduler config",
