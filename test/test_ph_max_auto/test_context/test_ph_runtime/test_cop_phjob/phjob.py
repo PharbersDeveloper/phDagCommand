49c4b8b509cfeecf58e1c806d9ac77a55865f1e6
hbzhao <15847059648@163.com>
2021-05-13 18:25:18 +0800

    s2cbug

---
+++ test/test_ph_max_auto/test_context/test_ph_runtime/test_cop_phjob/phjob.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""alfredyang@pharbers.com.
+
+This is job template for Pharbers Max Job
+"""
+
+from phcli.ph_logs.ph_logs import phs3logger, LOG_DEBUG_LEVEL
+
+
+def execute(**kwargs):
+    logger = phs3logger(kwargs["job_id"], LOG_DEBUG_LEVEL)
+    spark = kwargs['spark']()
+    result_path_prefix = kwargs["result_path_prefix"]
+    depends_path = kwargs["depends_path"]
+    a
+    ### input args ###
+    a = kwargs['a']
+    b = kwargs['b']
+    ### input args ###
+    
+    ### output args ###
+    c = kwargs['c']
+    d = kwargs['d']
+    ### output args ###
+
+    
+    logger.debug(a)
+    logger.debug(b)
+    logger.debug(c)
+    logger.debug(d)
+    return {}
