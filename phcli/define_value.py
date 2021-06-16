ce31b0c9e154f9ed58093bce0fc080ac64b2fd68
hbzhao <15847059648@163.com>
2021-06-16 16:43:08 +0800

    3.0.23 创建jupyter增加IP

--- phcli/define_value.py
+++ phcli/define_value.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-CLI_CLIENT_VERSION = '3.0.22'
+CLI_CLIENT_VERSION = '3.0.23'
 
 CLI_BUCKET = 'ph-platform'
 CLI_VERSION = "2020-11-11"
--- phcli/ph_auto_setup/jupyter_operation/ph_jupyter_operation.py
+++ phcli/ph_auto_setup/jupyter_operation/ph_jupyter_operation.py
@@ -20,20 +20,29 @@ class PhJupyterOperation(object):
         users = self.jupyter_name.split(",")
 
         # 从ssm获取cluster_id
-        ls_cmd = "aws ssm get-parameter --name cluster_id"
-        cluster_id_info = os.popen(ls_cmd).readlines()
+        cluster_id_cmd = "aws ssm get-parameter --name cluster_id"
+        cluster_id_info = os.popen(cluster_id_cmd).readlines()
         cluster_id_str = ''.join(cluster_id_info)
         cluster_id_dict = json.loads(cluster_id_str)
         cluster_id = cluster_id_dict['Parameter']['Value']
+
+        users_ip_cmd = "aws ssm get-parameter --name usersIp"
+        users_ip_info = os.popen(users_ip_cmd).readlines()
+        users_ip_str = ''.join(users_ip_info)
+        users_ip_dict = json.loads(users_ip_str)
+        users_ip = users_ip_dict['Parameter']['Value']
+        users_ip_dict = json.loads(users_ip)
 
         # 5.创建ec2实例
         create_jupyter_cmd1 = "curl https://ph-platform.s3.cn-northwest-1.amazonaws.com.cn/2020-11-11/emr/common/client/jupyterec2cfn.yaml -o ./jupyterec2cfn.yaml"
         os.system(create_jupyter_cmd1)
         for user in users:
+            user_ip = users_ip_dict.get(user, None)
             create_jupyter_cmd2 = "aws cloudformation create-stack --stack-name " + user + "-jupyter " \
                                                                                            "--template-body file://jupyterec2cfn.yaml " \
                                                                                            "--parameters ParameterKey=EMRClusterId,ParameterValue=" + cluster_id + \
-                                  " ParameterKey=EC2User,ParameterValue=" + user
+                                  " ParameterKey=EC2User,ParameterValue=" + user + \
+                                  " ParameterKey=PrivateIpAddress,ParameterValue=" + user_ip
             os.system(create_jupyter_cmd2)
         create_jupyter_cmd3 = "rm -f jupyterec2cfn.yaml"
         os.system(create_jupyter_cmd3)
